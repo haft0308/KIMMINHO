f81d9623088691671005c64cbf37e79cbe5e2d5d
kimminho <minho4386@gmail.com>
2022-03-18 16:29:32 +0900

    homework

---
+++ Ex10.java
@@ -0,0 +1,17 @@
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = -10;
+		if (x >= 0)
+		{
+			x = x;
+		}
+		else
+		{
+			x = -x;
+		}
+	}
+
+}
