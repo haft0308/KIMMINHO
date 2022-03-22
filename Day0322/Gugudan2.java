6a2d5d6da551171f9831a24edb8fb92116c95b1f
minomanimo <101791449+minomanimo@users.noreply.github.com>
2022-03-22 14:27:55 +0900

    Add files via upload

--- Day0322/Gugudan2.java
+++ Day0322/Gugudan2.java
@@ -5,6 +5,7 @@ public class Gugudan2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		System.out.println("단 수를 입력하세요.");
 		int num = sc.nextInt();
 		int i = 0, j = 0;
 		for (i = 1; i <= 9; i++)
