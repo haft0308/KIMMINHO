9fc17caeafd1a51bbfe11ed965a7705e649caa62
minomanimo <101791449+minomanimo@users.noreply.github.com>
2022-03-22 14:11:29 +0900

    Add files via upload

---
+++ src/Gugudan.java
@@ -0,0 +1,15 @@
+
+public class Gugudan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("3´Ü");
+		int i = 0, num = 0;
+		for (i = 1; i <=9; i++)
+		{
+			num = 3*i;
+			System.out.println("3*"+i+" = "+num);
+		}
+	}
+
+}
